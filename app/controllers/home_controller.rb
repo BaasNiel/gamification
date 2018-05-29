948b933700e2d22f1f3a6d199d2383a7cde7c5ba
Niel Sarvari <nielsarvari@gmail.com>
2018-05-29 19:38:06 +0200

    Some routing logic resolved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    
+
+    if params[:id].present?
+      @id = params[:id]
+    else
+      @id = current_user.id
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get "home/index"
+  get "home", to: "home#index"
+  get "home/:id", to: "home#index"
+
   root "home#index"
 end
