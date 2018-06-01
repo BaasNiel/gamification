9d8285108741343e7c0e296e822978fdf3429b4b
Niel Sarvari <nielsarvari@gmail.com>
2018-06-01 12:02:41 +0200

    Invite user

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :achievements
   resources :users
-  devise_for :users
 
   get "profile/index"
   get "profile", to: "profile#index"
   get "profile/:id", to: "profile#index"
-
-  # get "users", to: "users#index"
-  # get "users/:id", to: "users#show"
-  # get "users/new", to: "users#new"
-  # get "users/:id/edit", to: "users#edit"
-  # post "users", to: "users#create"
-  # delete "users/:id", to: "users#destroy"
-  # patch "users/:id", to: "users#update"
 
   root "profile#index"
 end
