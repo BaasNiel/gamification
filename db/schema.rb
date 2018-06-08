8f4a91fedcb5e70ad8075f6aca347886306f31d5
Niel Sarvari <nielsarvari@gmail.com>
2018-06-08 11:53:27 +0200

    Added joining table for user achievements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604085252) do
+ActiveRecord::Schema.define(version: 20180608094853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20180604085252) do
     t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "achievements_users_", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "achievement_id"
+    t.datetime "achieved_on_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["achievement_id"], name: "index_achievements_users__on_achievement_id"
+    t.index ["user_id"], name: "index_achievements_users__on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
