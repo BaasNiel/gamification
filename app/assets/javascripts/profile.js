eb7ffd55dc35ade6d4032efbf9d2ad2b1d016212
Niel Sarvari <nielsarvari@gmail.com>
2018-06-11 10:50:19 +0200

    Some date formatting on the profile page

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,7 +1,19 @@
+// Main date on profile/dashboard page. Format using MomentJS
 $(function() {
   let $el = $("#today-date");
   let text = $el.text();
   let momentDate = moment(text).format('dddd, DD MMMM');
 
   $el.text(momentDate);
+});
+
+// Every achievement in the list should have a "X days ago"
+// Formatted with MomentJS
+$(function() {
+  $('.time-ago').each(function(key) {
+    let momentDate = moment($(this).text()).fromNow();
+    let formattedDate = moment($(this).text()).format('lll');
+    $(this).text(momentDate);
+    $(this).attr('title', formattedDate);
+  });
 });
