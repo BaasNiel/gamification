df121a5f24123ac20e878327c9051359a9c37c26
Niel Sarvari <nielsarvari@gmail.com>
2018-06-11 12:40:21 +0200

    Remove user from team coz Im dumb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180611103337) do
+ActiveRecord::Schema.define(version: 20180611103852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,8 @@ ActiveRecord::Schema.define(version: 20180611103337) do
     t.string "name"
     t.string "avatar"
     t.string "start_of_week"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
   create_table "user_achievements", force: :cascade do |t|
@@ -76,5 +74,4 @@ ActiveRecord::Schema.define(version: 20180611103337) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "teams", "users"
 end
