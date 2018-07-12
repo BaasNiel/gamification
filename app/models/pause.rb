4ee8ee43afb37256a414dfcc564369d6a3a22863
Niel Sarvari <nielsarvari@gmail.com>
2018-07-12 13:13:00 +0200

    Code for pomodoro model and pause model

---
+++ app/models/pause.rb
@@ -0,0 +1,15 @@
+class Pause < ApplicationRecord
+  belongs_to :pomodoro
+
+  def active?
+    end_time.nil?
+  end
+
+  def complete
+    update!(end_time: Time.zone.now) unless end_time
+  end
+
+  def duration
+    ((end_time || Time.zone.now) - start_time).to_i
+  end
+end
