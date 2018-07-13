d2dac799c223ea4cabd3b875597fa73e29196efc
Niel Sarvari <nielsarvari@gmail.com>
2018-07-13 08:01:41 +0200

    Create, show and destroy pom

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -2,4 +2,26 @@ class PomodorosController < ApplicationController
   def index
     @pomodoros = Pomodoro.where user: current_user
   end
+
+  def create
+    new_pomodoro = Pomodoro.start(current_user)
+
+    if (new_pomodoro)
+      StopPomodoroService.create(new_pomodoro)
+      redirect_to pomodoro_path new_pomodoro
+    else
+      message = "A timer have already been started."
+      redirect_to pomodoros_path, flash: { error: message }
+    end
+  end
+
+  def show
+    @pomodoro = Pomodoro.find params[:id]
+  end
+
+  def destroy
+    p = Pomodoro.find(params[:id])
+    p.destroy!
+    redirect_to pomodoros_path
+  end
 end
