7a1710dd4257c9787d184b3f26cf7bb3ef02924b
Niel Sarvari <nielsarvari@gmail.com>
2018-07-16 21:57:23 +0200

    Show points for last week

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,11 +1,31 @@
 class ProfileController < ApplicationController
+  before_action :set_user
+
   def index
-    if params[:id].present?
+    @user_achievements = @user.user_achievements.order("date_achieved DESC, created_at DESC")
+    @week_string = get_week_string
+  end
+
+  # This function returns a string that displays the current week range,
+  # based on the team's first-day-of-week setting
+  def get_week_string
+    team = @user.team
+    start_of_week = Time.now.beginning_of_week(team.start_of_week_symbol)
+    end_of_week = Time.now.end_of_week(team.start_of_week_symbol)
+
+    start_string = start_of_week.strftime("%d %b %Y")
+    end_string = end_of_week.strftime("%d %b %Y")
+
+    start_string + " - " + end_string
+  end
+
+  def set_user
+    if @user
+      @user
+    elsif params[:id].present?
       @user = User.find params[:id]
     else
       @user = current_user
     end
-
-    @user_achievements = @user.user_achievements.order("date_achieved DESC, created_at DESC")
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,14 @@ class Team < ApplicationRecord
   has_many :users
   has_many :achievements
   has_one :admin, class_name: "User"
+
+  # Returns a symbol for the team's start of week.
+  # If nothing is set, :monday is returned
+  def start_of_week_symbol
+    if self.start_of_week.present?
+      self.start_of_week.to_sym
+    else
+      :monday
+    end
+  end
 end
