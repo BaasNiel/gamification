43548f4f96b593d4159edec873f8a7dca3a1ebc5
Niel Sarvari <nielsarvari@gmail.com>
2018-07-16 20:39:27 +0200

    Caused user achievements to be removed if an achievement is deleted

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,5 +1,5 @@
 class Achievement < ApplicationRecord
-  has_many :user_achievements
+  has_many :user_achievements, dependent: :destroy
   has_many :users, :through => :user_achievements
 
   belongs_to :team
