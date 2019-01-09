7dcba423bf890f20805c56b05454bd16696fef1d
Niel Sarvari <nielsarvari@gmail.com>
2019-01-09 15:21:32 +0200

    Create new sprint

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -10,4 +10,30 @@ class SprintsController < ApplicationController
   def new
     @sprint = Sprint.new
   end
+
+  def create
+    @sprint = Sprint.new(sprint_params)
+
+    respond_to do |format|
+      if @sprint.save
+        if current_user.admin?
+          format.html { redirect_to sprints_path, notice: 'Sprint was successfully created.' }
+          format.json { render :show, status: :created, location: @sprint }
+        else
+          format.html { redirect_to sprints_path, notice: 'Only the admin can create sprints.' }
+          format.json { render :show, status: :created, location: @sprint }
+        end
+      else
+        format.html { render :new }
+        format.json { render json: @sprint.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def sprint_params
+      params.require(:sprint).permit(:user_id, :start_date, :end_date, :expected_score, :actual_score)
+    end
 end
